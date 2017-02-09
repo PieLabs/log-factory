4271b0aee3dc0f017a0bdc31b44db86b60cfddf7
Ed Eustace <ed.eustace@gmail.com>
2017-02-09 12:31:17 +0000

    rm test dir

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const shell = require('gulp-shell');
+const runseq = require('run-sequence');
+const tslint = require('gulp-tslint');
+const { remove } = require('fs-extra');
+const merge = require('merge2');
+
+const paths = {
+  tscripts: {
+    src: ['src/**/*.ts'],
+    dest: 'lib'
+  }
+};
+
+gulp.task('default', done => runseq('clean', 'build', done));
+
+// ** Running ** //
+
+gulp.task('run', shell.task([
+  'node lib/index.js'
+]));
+
+gulp.task('clean', done => {
+  remove('./lib', done);
+});
+
+// ** Watching ** //
+
+gulp.task('watch', function () {
+  gulp.watch(paths.tscripts.src, ['compile:typescript']);
+});
+
+gulp.task('watchrun', function () {
+  gulp.watch(paths.tscripts.src, runseq('compile:typescript', 'run'));
+});
+
+let tsProject = ts.createProject('tsconfig.json');
+
+// ** Compilation ** //
+
+gulp.task('build', function () {
+  let tsResult = gulp.src('src/**/*.ts') // or tsProject.src()
+    .pipe(tsProject());
+
+  let js = tsResult.js
+    .pipe(gulp.dest('lib'));
+
+  let dts = tsResult.dts
+    .pipe(gulp.dest('lib'));
+
+  return merge([
+    js,
+    dts
+  ]);
+});
+
+
---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export declare let init: (log: any) => void;
+export declare let isLogLevel: (l: any) => boolean;
+export declare let setDefaultLevel: (l: any) => void;
+export declare let setConfigFromFile: (configPath: any) => void;
+export declare let setConfig: (cfg: any) => void;
+export declare let getLogger: (id: string) => any;
+export declare let fileLogger: (filename: any) => any;
+export declare function buildLogger(): any;
